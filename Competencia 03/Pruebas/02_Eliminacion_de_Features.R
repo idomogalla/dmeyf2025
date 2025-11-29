e126480a89572adf7c326d0faa11f3b9d3c6580d
Ignacio Domogalla <16903553+idomogalla@users.noreply.github.com>
2025-11-29 14:26:17 -0300

    Actualización Pruebas

--- Competencia 03/Pruebas/02_Eliminacion_de_Features.R
+++ Competencia 03/Pruebas/02_Eliminacion_de_Features.R
@@ -1,15 +1,25 @@
+eliminar_variables <- FALSE
+
 if (isTRUE(PARAM$eliminacion$prestamos_personales)) {
     log_info("Eliminando las variables mprestamos_personales, cprestamos_personales")
     dataset[, mprestamos_personales := NULL]
     dataset[, cprestamos_personales := NULL]
-} else if (isTRUE(PARAM$eliminacion$flip_internet)) {
+    eliminar_variables <- TRUE
+} 
+
+if (isTRUE(PARAM$eliminacion$flip_internet)) {
     log_info("Dando vuelta la variable internet")
     dataset[, internet := fifelse(internet %in% 0:1, as.integer(internet), NA_integer_)]
     dataset[foto_mes < 202010 & !is.na(internet), internet := 1L - internet]
-} else if (isTRUE(PARAM$eliminacion$internet)) {
+    eliminar_variables <- TRUE
+}
+
+if (isTRUE(PARAM$eliminacion$internet)) {
     log_info("Eliminando la variable internet")
     dataset[, internet := NULL]
+    eliminar_variables <- TRUE
 } 
-else {
-    log_info("No se elimina ninguna variable.")
+
+if (!eliminar_variables) {
+    log_info("No se han eliminado o modificado variables.")
 }
